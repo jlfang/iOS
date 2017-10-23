6f3903eb08173123a3f9facb1b1b1d2d3bb57970
Chris Fang <jlfang07@gmail.com>
2017-10-23 08:11:30 +0800

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Jialue Fang on 7/30/17.
+//  Copyright © 2017 Chris Fang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentInDisplay = display.text!
+            display.text = textCurrentInDisplay + digit
+        }
+        else {
+            display.text = digit
+            userIsInTheMiddleOfTyping = true
+        }
+        
+    }
+    
+    var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
+    }
+    
+    //load Model
+    private var brain = CalculatorBrain()
+    
+    @IBAction func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+      
+        if let mathematicalSymbol = sender.currentTitle {
+            brain.performOperation(mathematicalSymbol)
+        }
+        
+        if let result = brain.result {
+            displayValue = result
+        }
+        
+    }
+    
+    
+    
+}
+
